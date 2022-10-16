7fb3a2a17891d2eb55aa35eb74c2faed5aa31364
Михайло Мельник <melnyk.mykhailo@donnu.edu.ua>
2022-10-16 23:28:53 +0300

    upd gitignore

--- src/store/users.js
+++ src/store/users.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-//TODO: I think you need to separate Users store from authetnication store
+//TODO: I think you need to separate Users store from authetnication store ff
 export default {
   state: {
     userList: [],
